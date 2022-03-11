e3c0feb58004d0fb113ac972246568d01d1e6de6
Your Name <you@example.com>
2022-03-11 00:00:26 -0500

    styles form

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,15 +1,94 @@
-import './App.css';
+import "./sass/main.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser, faCircleCheck, faExclamationTriangle} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    
+    <main>
+     
+      <form action="">
+        <div className="input">
+        
+        <label htmlFor="">Usuario</label>
+        <div className="input__user">
+        <FontAwesomeIcon className="icon-first" icon={faUser} />
+        <input type="text" placeholder="usuario"/>
+        <FontAwesomeIcon className="icon-check" icon={faCircleCheck}/>
+        
+        </div>
+
+        <p>lorem</p>
+        
+        </div>
+        
+        {/* <div>
+        <label htmlFor="">Nombre</label>
+        <div>
+        <input type="text" placeholder="Nombre"/>
+        </div>
+          
+          <p>lorem</p>
+        </div>
+        {/*---------*/}
+        {/* <div>
+        <label htmlFor="">Contraseña</label>
+        <div>
+        <input type="text" placeholder="Contraseña"/>
+        </div>
+         
+          
+          <p>lorem</p>
+        </div>
+        {/*-----------*/}
+        {/* <div>
+        <label htmlFor="">Repetir contraseña</label>
+        <div>
+        <input type="text" placeholder="contraseña"/>
+        </div>
+
+          <p>lorem</p>
+        </div> */}
+        {/*-----------*/}
+        {/* <div>
+        <label htmlFor="">correo electronico</label>
+        <div>
+        <input type="text" placeholder="email"/>
+        </div>
+          
+          <p>lorem</p>
+        </div> */}
+        {/*----------*/}
+        {/* <div>
+        <label htmlFor="">telefono</label>
+        <div>
+        <input type="tel"  placeholder="número telefono"/>
+        </div>
+       
+          <p>lorem</p>
+        </div>  */}
+        
+        <div>
+          <label>
+            <input type="checkbox" name="terminos" id="terminos"></input>
+            Acepto los terminos y condiciones
+          </label>
+
+      </div> 
+      <div className="terminos">
         <p>
-          Edit 
+          <FontAwesomeIcon icon={faExclamationTriangle}/>
+          <b>Error:</b> Por favor rellene los campos obligatorios
         </p>
-      </header>
-    </div>
+      </div>
+      <div  className="container-btn">
+        <button className="container-btn__submit" type="submit">
+          Enviar
+        </button>
+        <p>formulario enviado con exito</p>
+      </div>
+      </form>    
+    </main>
+    
   );
 }
 
