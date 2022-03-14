bf9ef0cf1099ba2403c641bc1a4cb097fc829225
Your Name <you@example.com>
2022-03-14 11:25:50 -0500

    function validate

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,39 +1,58 @@
 import "./sass/main.scss";
+import React, { useState } from "react";
 import Inputs from "./components/Input";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUser,
-    faCircleCheck,
+  faCircleCheck,
   faExclamationTriangle,
-  faLock
+  faCircleXmark,
+  faLock,
 } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [user, updateUser] = useState({ empty: "", validate: null });
+  // const [name, setStateName] = useState({empty:'' , validate:null});
+  // const [password, setStatePassword] = useState({empty:'' , validate:null});
+  // const [password2, setStatePassword2] = useState({empty:'' , validate:null});
+  // const [email, setStateemail] = useState({empty:'' , validate:null});
+  // const [phone, setStatePhone] = useState({empty:'' , validate:null});
+
+  const expresiones = {
+    users: /^[a-zA-Z0-9.-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    names: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    passwords: /^.{4,12}$/, // 4 a 12 digitos.
+    emails: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    phones: /^\d{7,14}$/, // 7 a 14 numeros.
+  };
+
   return (
     <main>
-      <form action="">
-      
+      <form className="form" action="">
         <Inputs
-        icon = {faUser}
-        type = "text"
-        label="Usuario"
-        placeholder="Carla123"
-        name="usuario"
-        error = "Nombre del usuario no mennor de 4 caracteres y no mayor a 14 caracteres, solo puede contener, numeros, letras y guion bajo"
-        regex = {faCircleCheck}
+          state={user}
+          updateState={updateUser}
+          icon={faUser}
+          type="text"
+          label="Usuario"
+          placeholder="Carla123"
+          name="usuario"
+          error="Nombre del usuario no mennor de 4 caracteres y no mayor a 14 caracteres, solo puede contener, numeros, letras y guion bajo"
+          succes="campo correcto"
+          iconCheck={faCircleCheck}
+          iconMark={faCircleXmark}
+          regexInput={expresiones.users}
         />
-
-        <Inputs 
-        icon = {faLock}
-        type = "number"
-        label = "Contraseña"
-        placeholder = "******"
-        name="contraseña"
-        error = ""
-        regex={faCircleCheck}
-        />
-        
-
+        {/* 
+        <Inputs
+          icon={faLock}
+          type="number"
+          label="Contraseña"
+          placeholder="******"
+          name="contraseña"
+          error=""
+          regex={faCircleCheck}
+        /> */}
 
         <div className="terminos">
           <label className="terminos__label">
@@ -46,23 +65,25 @@ function App() {
             Acepto los terminos y condiciones
           </label>
         </div>
-       { false && <div className="container-error">
-          <p className="container-error__error">
-            <FontAwesomeIcon icon={faExclamationTriangle} />
-            <b>Error:</b> Por favor rellene los campos obligatorios
-          </p>
-        </div>}
+        {false && (
+          <div className="container-error">
+            <p className="container-error__error">
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+              <b>Error:</b> Por favor rellene los campos obligatorios
+            </p>
+          </div>
+        )}
         <div className="container-btn">
           <button className="container-btn__submit" type="submit">
             Enviar
           </button>
-          <p className="container-btn__msmexito">formulario enviado con exito</p>
+          <p className="container-btn__msmexito">
+            formulario enviado con exito
+          </p>
         </div>
       </form>
     </main>
   );
-
-  
 }
 
 export default App;
--- project1/src/components/Input.js
+++ project1/src/components/Input.js
@@ -1,25 +1,59 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const Inputs = (props) => {
+  const {
+    state = {
+      empty: "",
+      validate: null,
+    },
+    updateState,
+    label,
+    placeholder,
+    type,
+    name,
+    error,
+    succes,
+    icon,
+    iconMark,
+    iconCheck,
+    regexInput,
+  } = props;
+  console.log(state);
+  const onChange = (e) => {
+    updateState({ ...state, empty: e.target.value });
+  };
 
-const Inputs =  (props) => {
-    const{label,placeholder, type, name, error, regex, icon} = props;
+  const validation = () =>
+    regexInput.test(state.empty)
+      ? updateState({ ...state, validate: "true" })
+      : updateState({ ...state, validate: "false" });
 
-    return (
-        <div className="input">
-          <label htmlFor={name}>{label}</label>
-          <div className="input__user">
-            <FontAwesomeIcon className="icon-first" icon={icon} />
-            <input type={type} placeholder={placeholder} id={name} />
-            <FontAwesomeIcon className="icon-check" icon={icon} />
-          </div>
-
-          <p>{error}</p>
-        </div>
-
-    )
-     
-    
-
-}
+  return (
+    <div className="form-input">
+      <label htmlFor={name} validate={state.validate}>
+        {label}
+      </label>
+      <div className="form-input__user">
+        <FontAwesomeIcon className="icon-first" icon={icon} />
+        <input
+          type={type}
+          placeholder={placeholder}
+          id={name}
+          defaultValue={state.empty}
+          onChange={onChange}
+          onKeyUp={validation}
+          onBlur={validation}
+          validate={state.validate}
+        />
+        {state.validate === "true" ? (
+          <FontAwesomeIcon className="icon-check" icon={iconCheck} />
+        ) : (
+          <FontAwesomeIcon className="icon-mark" icon={iconMark} />
+        )}
+      </div>
+      {state.validate === "true" ? <p>{succes}</p> : <p>{error}</p>}
+    </div>
+  );
+};
 
 export default Inputs;
